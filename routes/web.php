06832fcab24d82bb9e0699b3fe131ea9792ff9fe
NikolayOskin <wskeal@gmail.com>
2019-01-03 03:02:18 +0300

    Updating routes

--- routes/web.php
+++ routes/web.php
@@ -11,39 +11,4 @@
 |
 */
 
-Route::get('/', function () {
-    $elastic = app(App\Search\Elastic::class);
-
-
-    $instance = new App\Post;
-
-    $items = $elastic->search([
-        'index' => $instance->getSearchIndex(),
-        'type' => $instance->getSearchType(),
-        'body' => [
-            'query' => [
-                'multi_match' => [
-                    'fields' => ['title', 'body'],
-                    'query' => 'experiment',
-                ],
-            ],
-            'highlight' => [
-                'pre_tags' => ["<b>"],
-                'post_tags' => ["</b>"],
-                'fields' => [
-                    'body' => new \stdClass()
-                ],
-
-            ],
-        ],
-    ]);
-    //dd($items);
-    $hits = array_pluck($items['hits']['hits'], '_source') ?: [];
-    dd(App\Post::hydrate($hits));
-
-
-
-    dd($hits);
-
-    return view('welcome');
-});
+Route::get('/', 'SearchController@searchPosts');
